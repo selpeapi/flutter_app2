fec69cf21fe350b9a0944b3bc52e467573035a79
selpeapi <selpeapi@gmail.com>
2020-10-30 21:34:32 +0900

    텍스트 설정2

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,13 @@ class _MainPageState extends State<MainPage> {
                                                           children: [
                                                             Text("asd"),
                                                             Text("qwer"),
-                                                            Text("aaaa"),
+                                                            Text(
+                                                              "aaaa",
+                                                              style: TextStyle(
+                                                                color: Colors
+                                                                    .green,
+                                                              ),
+                                                            ),
                                                           ],
                                                         ),
                                                       )),
