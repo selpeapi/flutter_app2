bd1f3577956161fd560dd1d562b95c26ed5d5a76
selpeapi <selpeapi@gmail.com>
2020-11-02 22:24:38 +0900

    텍스트 설정4_8 +상위 3번째 위젯의 Text 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -74,20 +74,8 @@ class _MainPageState extends State<MainPage> {
                                                                     const EdgeInsets
                                                                             .all(
                                                                         8.0),
-                                                                child: Text(
-                                                                  "aaaa",
-                                                                  style: TextStyle(
-                                                                      color: Colors
-                                                                          .green,
-                                                                      backgroundColor:
-                                                                          Colors
-                                                                              .white24,
-                                                                      fontSize:
-                                                                          20),
-                                                                ),
                                                               ),
                                                             ),
-
                                                           ],
                                                         ),
                                                       )),
